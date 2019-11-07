3e7ebffb4d297926ecf362239fdad514df81e563
Elias Svoba <elisvobs@live.com>
2019-11-07 13:41:09 +0000

    Second commit

--- app/src/main/java/com/elisvobs/cropcalendar/MainActivity.java
+++ app/src/main/java/com/elisvobs/cropcalendar/MainActivity.java
@@ -2,7 +2,9 @@ package com.elisvobs.cropcalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewCrops(View view) {
+        int id = view.getId();
+        switch(id) {
+            case R.id.bean:
+//                startActivity(new Intent(this, BeanActivity.class));
+                break;
+            case R.id.beet:
+//                startActivity(new Intent(this, BeetActivity.class));
+                break;
+            case R.id.brassica:
+//                startActivity(new Intent(this, BrassicaActivity.class));
+                break;
+            case R.id.carrot:
+//                startActivity(new Intent(this, CarrotActivity.class));
+                break;
+            case R.id.daisy:
+//                startActivity(new Intent(this, DaisyActivity.class));
+                break;
+            case R.id.cucurbit:
+//                startActivity(new Intent(this, CucurbitActivity.class));
+                break;
+            case R.id.onion:
+//                startActivity(new Intent(this, OnionActivity.class));
+                break;
+            case R.id.potato:
+//                startActivity(new Intent(this, PotatoActivity.class));
+                break;
+        }
+
     }
 }
